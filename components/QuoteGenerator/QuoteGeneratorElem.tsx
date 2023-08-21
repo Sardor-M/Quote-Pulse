a67075325d2000d88ca6b3de3c442d1cbbaadece
Sardor Madaminov <sardor0968@gmail.com>
2023-08-21 17:32:56 +0900

    footer and img components: added

--- components/QuoteGenerator/QuoteGeneratorElem.tsx
+++ components/QuoteGenerator/QuoteGeneratorElem.tsx
@@ -20,3 +20,33 @@ export const GradientBackgroundCon = styled.div`
     }
   }
 `;
+
+export const BackgorundCloudImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: 30px;
+  margin-top: -10px;
+`;
+export const BackgorundCloudImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: 0;
+  bottom: 25px;
+`;
+
+export const FooterContainer = styled.div`
+  width: 100vw;
+  height: 50px;
+  background-color: #7b7be2;
+  text-align: center !important;
+  font-family: "Source Code Pro", monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: #fff;
+  z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+  color: #fff;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElem";
+
+// Components
+import {
+  BackgorundCloudImage1,
+  BackgorundCloudImage2,
+  FooterContainer,
+  FooterLink,
+  GradientBackgroundCon,
+} from "@/components/QuoteGenerator/QuoteGeneratorElem";
+
+// Assets
+import Cloud_1 from "assets/cloud-image-here.png";
+import Cloud_2 from "assets/cloud-image-new.png";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -12,8 +28,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GradientBackgroundCon>
-        <div>Hello Quote-Pulse</div>
+        <BackgorundCloudImage1 src={Cloud_1} alt="cloudyImage1" height="300" />
+        <BackgorundCloudImage2 src={Cloud_2} alt="cloudyImage2" height="300" />
       </GradientBackgroundCon>
+      <FooterContainer>
+        <>
+          Quotes generated: {numberOfQuotes}
+          <br />
+          Developed with ❤️ by{" "}
+          <FooterLink
+            href="https://github.com/sardor-m"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @Sardor-M
+          </FooterLink>
+        </>
+      </FooterContainer>
     </>
   );
 }
