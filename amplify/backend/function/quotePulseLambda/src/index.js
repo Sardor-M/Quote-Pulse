9c88dd0d1bba9db1f2156c4dfac7497fe5c1532e
Sardor Madaminov <sardor0968@gmail.com>
2023-10-03 20:17:47 +0900

    XML data prasing minor issue: fixed

--- amplify/backend/function/quotePulseLambda/src/index.js
+++ amplify/backend/function/quotePulseLambda/src/index.js
@@ -75,7 +75,7 @@ exports.handler = async (event) => {
     const width = 750;
     const height = 483;
     const text = quoteText;
-    const words = text.split("");
+    const words = text.split(" ");
     const lineBreak = 4;
     let newText = "";
 
@@ -86,12 +86,12 @@ exports.handler = async (event) => {
       if ((i + 1) % lineBreak === 0) {
         tspanElements += `<tspan x="${
           width / 2
-        }" dy="1.2em">${newText}</tspan> `;
+        }" dy="1.2em">${newText}</tspan>`;
         newText = "";
       }
     }
     if (newText !== "") {
-      tspanElements += `<tspan x="${width / 2}" dy="1.2em">${newText}</tspan> `;
+      tspanElements += `<tspan x="${width / 2}" dy="1.2em">${newText}</tspan>`;
     }
     console.log(tspanElements);
 
@@ -99,37 +99,37 @@ exports.handler = async (event) => {
     const svgImg = `
       <svg width="${width}" height="${height}">
           <style>
-              .title{
-                  fill: #ffffff;
-                  font-size: 20px;
-                  font-weight: bold;
-              }     
-              .quoteAuthorStyles{
-                  font-size: 35px;
-                  font-weight: bold;
-                  padding: 50px;
-              }
-              .footerStyles {
-                  font-size: 20px;
-                  font-weight: bold;
-                  fill: lightgrey;
-                  text-anchor: middle;
-                  font-family: Verdana;
-              }
-              </style>
-              <circle cx="382" cy="76" r="44" fill="rgba(255, 255, 255, 0.155)"/>
-              <text x="382" y="76" dy="50" text-anchor="middle" font-size="90" font-family="Verdana" fill=""white">"</text>
-              <g>
-                  <rect x="0" y="0" width="${width}" height="auto"> </rect>
-                  <text id="lastLineOfQuote" x="375" y="120" font-family="Verdana" font-size="35" fill="white" text-anchor="middle"> 
-                      ${tspanElements}
-                      <tspan class="quoteAuthorStyles" x="375" dy="1.8em">- ${quoteAuthor}</tspan>          
-                  </text>
-              </g>
-              <text x="${width / 2}" y="${
+            .title {
+                fill: #ffffff;
+                font-size: 20px;
+                font-weight: bold;
+            }     
+            .quoteAuthorStyles {
+                font-size: 35px;
+                font-weight: bold;
+                padding: 50px;
+            }
+            .footerStyles {
+                font-size: 20px;
+                font-weight: bold;
+                fill: lightgrey;
+                text-anchor: middle;
+                font-family: Verdana;
+            }
+            </style>
+            <circle cx="382" cy="76" r="44" fill="rgba(255, 255, 255, 0.155)"/>
+            <text x="382" y="76" dy="50" text-anchor="middle" font-size="90" font-family="Verdana" fill=""white">"</text>
+            <g>
+                <rect x="0" y="0" width="${width}" height="auto"></rect>
+                <text id="lastLineOfQuote" x="375" y="120" font-family="Verdana" font-size="35" fill="white" text-anchor="middle"> 
+                    <tspan x="${width / 2}" dy="1.2em">${tspanElements}</tspan>
+                    <tspan class="quoteAuthorStyles" x="375" dy="1.8em">-${quoteAuthor}</tspan>          
+                </text>
+            </g>
+            <text x="${width / 2}" y="${
       height - 10
-    }" class="footerStyles"> Developed by @Sardor-M | Quotes from ZenQuotes.io </text>
-      </svg>
+    }" class="footerStyles"> Developed by @Sardor-M | Quotes from ZenQuotes.io</text>
+    </svg>
     `;
 
     // add background images for the svg creation
@@ -160,7 +160,7 @@ exports.handler = async (event) => {
 
     // function to update the quote object in DynomoDB
     try {
-      updateQuoteDBObject();
+      updateDynomoDBObject();
     } catch (error) {
       console.log("error in updating the quote object in DynomoDB", error);
     }
